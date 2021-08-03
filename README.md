d87cab8f022b1c93a158ba70b7b33bb7fc2927f7
Gautam Anand <gautamanand022@gmail.com>
2021-08-03 13:29:32 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Clean React App
+
+    ~~this is a cleaner template than npm's create-react-app~~
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <code>
+        <h3>Start Editing In src/App.js</h3>
+      </code>
+    </div>
+  );
+};
+
+export default App;
